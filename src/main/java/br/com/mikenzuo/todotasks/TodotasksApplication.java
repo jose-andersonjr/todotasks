e2b999beb05f1068f331b11db590fc44c67ce6bb
José Anderson <jose.freitas@netbiis.com>
2024-10-11 12:26:28 -0300

    :bookmark: Initial commit, adding essencial file to run project

---
+++ src/main/java/br/com/mikenzuo/todotasks/TodotasksApplication.java
@@ -0,0 +1,13 @@
+package br.com.mikenzuo.todotasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodotasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodotasksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mikenzuo/todotasks/TodotasksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mikenzuo.todotasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodotasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
